cb747d57bc3c62daf6653199190389288790e681
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-03-20 12:56:37 +0530

    Spinner update

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { client } from "../client";
+import MasonaryLayout from "./MasonaryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  if (loading) return <Spinner message={`We are updating your Feeds!`} />;
   return <div>Feed</div>;
 };
 
---
+++ src/components/MasonaryLayout.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import Spinner from "./Spinner";
+
+const MasonaryLayout = () => {
+  const [loading, setLoading] = useState(false);
+
+  return <div>MasonaryLayout</div>;
+};
+
+export default MasonaryLayout;
