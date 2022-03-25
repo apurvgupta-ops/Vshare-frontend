01746037efb8f1d5b153943c842f04098c613fb3
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-03-25 22:15:19 +0530

    Bug fix (comments and image loading)

--- src/components/PinDetails.js
+++ src/components/PinDetails.js
@@ -16,10 +16,11 @@ const PinDetails = ({ user }) => {
   const [addingComment, setAddingComment] = useState(false);
 
   const fetchingDetails = () => {
-    const query = pinDetailQuery();
+    const query = pinDetailQuery(pinId);
     if (query) {
       client.fetch(query).then((data) => {
-        setPinDetails(query);
+        setPinDetails(data[0]);
+        // console.log(query);
         console.log(data);
         if (data[0]) {
           const query1 = pinDetailMorePinQuery(data[0]);
@@ -38,10 +39,11 @@ const PinDetails = ({ user }) => {
   const addComments = () => {
     if (comment) {
       setAddingComment(true);
+      // console.log(comment);
 
       client
         .patch(pinId)
-        .setIfMissing({ comments: [] })
+        .setIfMissing({ comment: [] })
         .insert("after", "comments[-1]", [
           {
             comment,
@@ -80,7 +82,7 @@ const PinDetails = ({ user }) => {
             <div className="flex items-center justify-between">
               <div className="flex gap-2 items-center">
                 <a
-                  href={`${pinDetails?.image?.asset.url}?dl=`}
+                  href={`${pinDetails.image.asset.url}?dl=`}
                   download
                   className="bg-secondaryColor p-2 text-xl rounded-full flex items-center justify-center text-dark opacity-75 hover:opacity-100"
                 >
@@ -110,7 +112,7 @@ const PinDetails = ({ user }) => {
             </Link>
             <h2 className="mt-5 text-2xl">Comments</h2>
             <div className="max-h-370 overflow-y-auto">
-              {pinDetails?.comment?.map((item) => (
+              {pinDetails?.comments?.map((item) => (
                 <div
                   className="flex gap-2 mt-5 items-center bg-white rounded-lg"
                   key={item.comment}
@@ -128,7 +130,7 @@ const PinDetails = ({ user }) => {
               ))}
             </div>
             <div className="flex flex-wrap mt-6 gap-3">
-              <Link to={`/user-profile/${user._id}`}>
+              <Link to={`/user-profile/${user?._id}`}>
                 <img
                   src={user.image}
                   className="w-10 h-10 rounded-full cursor-pointer"
